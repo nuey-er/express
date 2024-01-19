eaa8dc16b4a64b997109a43564293908f7926805
nuey-er <jiabenjunjie124@gmail.com>
2024-01-19 13:29:10 +0900

    add env

--- routes/notes_from_c.js
+++ routes/notes_from_c.js
@@ -1,25 +1,24 @@
 var express = require('express');
 var router = express.Router();
-const cors=require('cors');
-require('dotenv').config();
+const cors = require('cors'); // corsミドルウェアを追加
+
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://*************@cluster0.4r0nywv.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
-
-//corsミドルウェアを使用
-router.use.apply(cors());
+// corsミドルウェアを使用
+router.use(cors());
 
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが１のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
 
 res.json(note);
 })
